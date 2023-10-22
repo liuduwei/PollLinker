75ff373b1c45c71959dcf617084e4e3a2beb69c0
liuduwei <17341730328@163.com>
2023-10-22 20:20:48 +0800

    测试网站uri

--- src/api/index.js
+++ src/api/index.js
@@ -4,9 +4,10 @@ const client_secret = "a5bd128c703c6401c31a6605b3f48f9f";
 const client_id = "3828707668";
 const grant_type = "authorization_code";
 /*redirect_uri convert by https://anspoon.com/url-conveter use convert value*/
-const redirect_uri_convert = "http%3A%2F%2Flocal.polllinker.com%2F%23%2Flogin";
+const redirect_uri_convert = "http%3A%2F%2Fyousan.xyz%2F%23%2Flogin";
 // const redirect_uri = "http://yousan.xyz";
-const redirect_uri = "http://local.polllinker.com/#/login";
+const redirect_uri = "http://yousan.xyz/#/login";
+
 export const authUrl = `https://api.weibo.com/oauth2/authorize?client_id=${client_id}&redirect_uri=${redirect_uri_convert}`;
 
 // 登陆
